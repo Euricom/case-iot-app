7b04f20fd5b81dc6c13fc922a7a7c8c201e01732
Kurt Torfs <kurt.torfs@euri.com>
2016-12-20 13:06:24 +0100

    Initial commit;

---
+++ IoT.Web/src/IoT.Web/Controllers/DoorController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace IoT.Web.Controllers
+{
+    [Route("api/[controller]")]
+    public class DoorController : Controller
+    {
+        // GET: api/door/status
+        [HttpGet("status")]
+        public IActionResult Status()
+        {
+            var next = new Random().Next(100);
+            return new ObjectResult((next % 2) == 0);
+        }
+
+        // POST api/door/open
+        [HttpPost("open")]
+        public IActionResult Open()
+        {
+            return Ok();
+        }
+
+        // POST api/door/close
+        [HttpPost("close")]
+        public IActionResult Close()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ IoT.Web/src/IoT.Web/Controllers/LightController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace IoT.Web.Controllers
+{
+    [Route("api/[controller]")]
+    public class LightController : Controller
+    {
+        // GET: api/light/status
+        [HttpGet("status")]
+        public IActionResult Status()
+        {
+            var next = new Random().Next(100);
+            return new ObjectResult((next % 2) == 0);
+        }
+
+        // POST api/light/on
+        [HttpPost("on")]
+        public IActionResult On()
+        {
+            return Ok();
+        }
+
+        // POST api/light/off
+        [HttpPost("off")]
+        public IActionResult Off()
+        {
+            return Ok();
+        }
+    }
+}
